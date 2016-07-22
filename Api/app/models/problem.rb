68dd9cee41992309915542a46da1fd924092d7fb
JosepRIvaille <josep.de.cid@gmail.com>
2016-07-22 11:55:30 +0200

    Removed old Rails API

--- Api/app/models/problem.rb
+++
@@ -1,5 +0,0 @@
-class Problem < ApplicationRecord
-  belongs_to :user
-  validates :title, presence: true
-  validates :description, presence: true
-end
--- Api/app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ApplicationRecord
-  has_many :problems
-  validates :name, :presence => true
-  validates :email, :presence => true
-  validates :password, :presence => true
-  validates :phone, :presence => true
-end
